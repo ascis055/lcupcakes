fad05233b6316d1ebdae4fa8c0f1f3c13609860e
Alexandra Stankovich <stankovichalexandra@gmail.com>
2024-08-23 23:47:22 -0700

    Initial commit

---
+++ src/main/java/com/github/ascis055/LCupcakes/LCupcakesApp.java
@@ -0,0 +1,17 @@
+package com.github.ascis055.LCupcakes;
+
+import java.util.*;
+import org.json.simple.*;
+import com.github.ascis055.LCupcakes.sort.*;
+
+/**
+ * Hello world!
+ *
+ */
+public class LCupcakesApp
+{
+    public static void main( String[] args )
+    {
+        MergeSort.main(args);
+    }
+}
